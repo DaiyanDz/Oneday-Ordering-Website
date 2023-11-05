a31e2a936586df1dbbbc2d6aef055cf73d4cc1a3
Muhammad Daiyan Dzafrani Bin Zambari <111353170+DaiyanDz@users.noreply.github.com>
2023-11-06 01:20:52 +0800

    Add files via upload

---
+++ Oneday Ordering/about.php
@@ -0,0 +1,131 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+<?php include 'components/user_header.php'; ?>
+<!-- header section ends -->
+
+<div class="heading">
+   <h3>About Us</h3>
+   <p><a href="index.php">Home</a> <span> / About</span></p>
+</div>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/onenew.jfif" alt="">
+      </div>
+
+      <div class="content">
+         <h3>Why Choose Us?</h3>
+         <p>We are using 100% arabican coffee beans</p>
+         <a href="menu.php" class="btn">our menu</a>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- steps section starts  -->
+
+<section class="steps">
+
+   <h1 class="title">simple steps</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/step-1.png" alt="">
+         <h3>Choose Order</h3>
+         <p>Variety of product available for our customer.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/step-3.png" alt="">
+         <h3>Pickup At Store</h3>
+         <p>Pickup at our store after you make purchase through online.</p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- steps section ends -->
+
+<!-- footer section starts  -->
+<?php include 'components/footer.php'; ?>
+<!-- footer section ends -->=
+
+
+
+
+
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   grabCursor: true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+      slidesPerView: 1,
+      },
+      700: {
+      slidesPerView: 2,
+      },
+      1024: {
+      slidesPerView: 3,
+      },
+   },
+});
+
+</script>
+
+</body>
+</html>
---
+++ Oneday Ordering/components/footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>Our Email</h3>
+         <a href="mailto:oneday@gmail.com">oneday@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Business Hours</h3>
+         <p>14:00pm to 11:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>Our Address</h3>
+         <a href="#">K650 Lorong Haji Wahab, Kampung Masjid, 08000 Sungai Petani, Kedah</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>Our Number</h3>
+         <a href="tel: 014-753 0035>014-753 0035</a>
+      </div>
+
+   </section>
+
+   <div class="credit"> 014-753 0035 <?= date('Y'); ?> | Mr. Wan Daniel</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
